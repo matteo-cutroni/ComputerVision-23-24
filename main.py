11ad69deed5bb4f894285fe4a003f23a3fd68e27
matteo cutroni <cutroni.1918336@studenti.uniroma1.it>
2024-07-08 20:37:48 +0200

    crop players

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from ultralytics import YOLO
 
 
-model = YOLO("yolo-Weights/yolov9e.pt") 
+model = YOLO("yolo-Weights/yolov9t.pt") 
 
-results = model.predict("calcio.mp4", save = True)
+results = model.track("barella.mov", save=True, stream=True)
 
-
+for result in results:
+    im = result.orig_img
+    for xywh in result.boxes.xywh:
+        x, y, w, h = [int(xywh[i]) for i in range(4)]
+        left = int(x-w/2)
+        top = int(y-h/2)
+        crop = result.orig_img[top:top+h, left:left+w]
