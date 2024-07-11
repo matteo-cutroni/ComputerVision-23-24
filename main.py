cc9807c0284086e9695ec2671fc6151ac01bbdf5
matteo cutroni <cutroni.1918336@studenti.uniroma1.it>
2024-07-11 23:50:27 +0200

    assegnazione squadre da sistemare

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import cv2 as cv
 from sklearn.cluster import KMeans
 import numpy as np
 from matplotlib import pyplot as plt
+import math
 
 model = YOLO("yolo-Weights/yolov9e.pt") 
 
 cap = cv.VideoCapture('barella.mov')
+
+team_colors = []
+player_color = {}
+
 
 while cap.isOpened():
     success, frame = cap.read()
@@ -15,6 +20,11 @@ while cap.isOpened():
         results = model.track(frame, persist=True)
 
         for i, xywh in enumerate(results[0].boxes.xywh):
+
+            # il colore è assegnato solo a nuovi id
+            if int(results[0].boxes.id[i]) in player_color:
+                continue
+
             x, y, w, h = [int(xywh[j]) for j in range(4)]
             left = int(x-w/2)
             top = int(y-h/2)
@@ -26,20 +36,44 @@ while cap.isOpened():
             # modifico in array 2d diviso per colori di pixel
             flat_crop = top_crop.reshape(-1, 3)
 
-            kmeans = KMeans(n_clusters=2, init='k-means++', n_init=1)
-            kmeans.fit(flat_crop)
+            kmeans_player = KMeans(n_clusters=2, init='k-means++', n_init=1)
+            kmeans_player.fit(flat_crop)
 
-            labels = kmeans.labels_
+            labels = kmeans_player.labels_
 
             # il colore della maglietta è quello della label meno presente
             tot = sum(labels)
-            if tot > flat_crop.shape[0]/2:
-                player_color = kmeans.cluster_centers_[0]
-            else:
-                player_color = kmeans.cluster_centers_[1]
 
-            cv.rectangle(frame, (left, top), (left+w, top+h), player_color, 2)
+            if tot > flat_crop.shape[0]/2:
+                player_color[int(results[0].boxes.id[i])] = kmeans_player.cluster_centers_[0]
+            else:
+                player_color[int(results[0].boxes.id[i])] = kmeans_player.cluster_centers_[1]
+
+
+        # al primo frame vengono assegnati i colori delle due squadre
+        if len(team_colors) == 0:
+
+            kmeans_team = KMeans(n_clusters=2, init='k-means++', n_init=1)
+            kmeans_team.fit(list(player_color.values()))
+
+            labels = kmeans_team.labels_
+
+            team_colors = kmeans_team.cluster_centers_
+
+        for i, xywh in enumerate(results[0].boxes.xywh):
+                        
+            x, y, w, h = [int(xywh[j]) for j in range(4)]
+            left = int(x-w/2)
+            top = int(y-h/2)
+
+            if player_color[int(results[0].boxes.id[i])] not in team_colors:
+
+                distances = [np.linalg.norm(player_color[int(results[0].boxes.id[i])] - color) for color in team_colors]
             
+                player_color[int(results[0].boxes.id[i])] = team_colors[np.argmin(distances)]
+
+            cv.rectangle(frame, (left, top), (left+w, top+h), player_color[int(results[0].boxes.id[i])], 2)
+            cv.putText(frame, f'ID: {int(results[0].boxes.id[i])}', (left, top+5), cv.FONT_HERSHEY_COMPLEX, 1, player_color[int(results[0].boxes.id[i])], 2)
 
         cv.imshow('bboxes', frame)
 
