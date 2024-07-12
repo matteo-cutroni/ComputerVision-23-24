e9763465738c9197f9ce37cff841675afe3f3930
matteo cutroni <cutroni.1918336@studenti.uniroma1.it>
2024-07-12 17:01:46 +0200

    squadre assegnate correttamente

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ while cap.isOpened():
         results = model.track(frame, persist=True)
 
         for i, xywh in enumerate(results[0].boxes.xywh):
-
+            # operazione da fare solo per le persone
+            if model.names[int(results[0].boxes.cls[i])] != 'person':
+                continue
             # il colore è assegnato solo a nuovi id
             if int(results[0].boxes.id[i]) in player_color:
                 continue
@@ -36,7 +38,7 @@ while cap.isOpened():
             # modifico in array 2d diviso per colori di pixel
             flat_crop = top_crop.reshape(-1, 3)
 
-            kmeans_player = KMeans(n_clusters=2, init='k-means++', n_init=1)
+            kmeans_player = KMeans(n_clusters=2, init='k-means++', n_init=1, random_state=42)
             kmeans_player.fit(flat_crop)
 
             labels = kmeans_player.labels_
@@ -53,23 +55,26 @@ while cap.isOpened():
         # al primo frame vengono assegnati i colori delle due squadre
         if len(team_colors) == 0:
 
-            kmeans_team = KMeans(n_clusters=2, init='k-means++', n_init=1)
+            kmeans_team = KMeans(n_clusters=2, init='k-means++', n_init=1, random_state=42)
             kmeans_team.fit(list(player_color.values()))
-
-            labels = kmeans_team.labels_
 
             team_colors = kmeans_team.cluster_centers_
 
+
         for i, xywh in enumerate(results[0].boxes.xywh):
-                        
+            # operazione da fare solo per le persone
+            if model.names[int(results[0].boxes.cls[i])] != 'person':
+                continue          
+
             x, y, w, h = [int(xywh[j]) for j in range(4)]
             left = int(x-w/2)
             top = int(y-h/2)
 
+            # colore assegnato solo a nuovi giocatori tracciati
             if player_color[int(results[0].boxes.id[i])] not in team_colors:
 
-                distances = [np.linalg.norm(player_color[int(results[0].boxes.id[i])] - color) for color in team_colors]
-            
+                # assegnato al giocatore il colore della squadra più vicino a quello della sua maglietta
+                distances = [np.linalg.norm(player_color[int(results[0].boxes.id[i])] - 1.5*color) for color in team_colors]
                 player_color[int(results[0].boxes.id[i])] = team_colors[np.argmin(distances)]
 
             cv.rectangle(frame, (left, top), (left+w, top+h), player_color[int(results[0].boxes.id[i])], 2)
